bd8a95dabc290e946da2f34197e6a51cb6251306
Alexey Glazkov <glazkov.dev@outlook.com>
2021-07-04 12:30:10 +0300

    Remove preinstall script

--- src/stories/Logo.stories.js
+++ src/stories/Logo.stories.js
@@ -1,12 +1,12 @@
-// /* eslint-disable @scandipwa/scandipwa-guidelines/jsx-no-props-destruction */
-// import React from 'react';
+/* eslint-disable @scandipwa/scandipwa-guidelines/jsx-no-props-destruction */
+import React from 'react';
 
-// import Logo from '../component/Logo/Logo.component';
+import Logo from '../component/Logo/Logo.component';
 
-// export default {
-//     title: 'ScandiPWA/Logo',
-//     component: Logo
-// };
+export default {
+    title: 'ScandiPWA/Logo',
+    component: Logo
+};
 
-// /** @namespace ScandipwaStorybook/Stories/Logo/Stories/Template */
-// export const Template = (args) => <Logo { ...args } />;
+/** @namespace ScandipwaStorybook/Stories/Logo/Stories/Template */
+export const Template = (args) => <Logo { ...args } />;
